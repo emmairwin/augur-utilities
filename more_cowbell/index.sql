2ada070411c0a7274ad3dc9c1500a67fc9444d29
Sean P. Goggins <s@goggins.com>
2025-04-02 15:29:56 -0500

    index verifications to maximize success
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- more_cowbell/index.sql
+++ more_cowbell/index.sql
@@ -2,5 +2,13 @@ ALTER TABLE "augur_data"."issue_assignees"
   DROP CONSTRAINT "fk_issue_assignees_issues_1",
   DROP CONSTRAINT "issue_assignees_cntrb_id_fkey",
   ADD CONSTRAINT "fk_issue_assignees_issues_1" FOREIGN KEY ("issue_id") REFERENCES "augur_data"."issues" ("issue_id") ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT "issue_assignees_cntrb_id_fkey" FOREIGN KEY ("cntrb_id") REFERENCES "augur_data"."contributors" ("cntrb_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT "issue_assignees_cntrb_id_fkey" FOREIGN KEY ("cntrb_id") REFERENCES "augur_data"."contributors" ("cntrb_id") ON DELETE RESTRICT ON UPDATE CASCADE  DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE "augur_data"."pull_request_review_message_ref" 
+DROP CONSTRAINT "fk_pull_request_review_message_ref_message_1",
+ADD CONSTRAINT "fk_pull_request_review_message_ref_message_1" FOREIGN KEY ("msg_id") REFERENCES "augur_data"."message" ("msg_id") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE "augur_data"."pull_request_message_ref" 
+DROP CONSTRAINT "fk_pull_request_message_ref_message_1",
+ADD CONSTRAINT "fk_pull_request_message_ref_message_1" FOREIGN KEY ("msg_id") REFERENCES "augur_data"."message" ("msg_id") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
 

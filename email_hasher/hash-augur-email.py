5d140358ca1104ebf3ed02dcc526300884532c0f
Sean P. Goggins <s@goggins.com>
2025-04-04 17:11:30 -0500

    update to schema parameter
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- email_hasher/hash-augur-email.py
+++ email_hasher/hash-augur-email.py
@@ -61,28 +61,28 @@ def main(secret_key):
 
         # Encrypt cmt_author_raw_email.
         cursor.execute("""
-            UPDATE commits
+            UPDATE augur_data.commits
             SET cmt_author_raw_email = encode(pgp_sym_encrypt(cmt_author_raw_email, %s), 'base64');
         """, (secret_key,))
         conn.commit()
 
         # Encrypt cmt_author_email.
         cursor.execute("""
-            UPDATE commits
+            UPDATE augur_data.commits
             SET cmt_author_email = encode(pgp_sym_encrypt(cmt_author_email, %s), 'base64');
         """, (secret_key,))
         conn.commit()
 
         # Encrypt cmt_committer_raw_email.
         cursor.execute("""
-            UPDATE commits
+            UPDATE augur_data.commits
             SET cmt_committer_raw_email = encode(pgp_sym_encrypt(cmt_committer_raw_email, %s), 'base64');
         """, (secret_key,))
         conn.commit()
 
         # Encrypt cmt_committer_email.
         cursor.execute("""
-            UPDATE commits
+            UPDATE augur_data.commits
             SET cmt_committer_email = encode(pgp_sym_encrypt(cmt_committer_email, %s), 'base64');
         """, (secret_key,))
         conn.commit()

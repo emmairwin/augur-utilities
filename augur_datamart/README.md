735d94701c356d2de0bb228ab3c3dca03369a41b
Sean P. Goggins <s@goggins.com>
2025-04-09 13:21:50 -0500

    updated with datamart table script first cut
    Signed-off-by: Sean P. Goggins <s@goggins.com>

---
+++ augur_datamart/README.md
@@ -0,0 +1,4 @@
+# This script truncates and repopulates Augur Datamart Tables
+
+Augur datamart tables are summaries of commit activity by year, month and week. All have the prefix `dm_` and are in the `augur_data` schema. 
+
---
+++ augur_datamart/datamart.py
@@ -0,0 +1,30 @@
+import json
+import psycopg2
+
+def load_db_config(filename='db.config.json'):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+def run_sql_script(sql, db_config):
+    try:
+        connection = psycopg2.connect(**db_config)
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+        print("SQL script executed successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+if __name__ == "__main__":
+    db_config = load_db_config()
+
+    # Optionally load from file to keep things clean
+    with open("bulk_insert.sql", "r") as f:
+        sql_script = f.read()
+
+    run_sql_script(sql_script, db_config)

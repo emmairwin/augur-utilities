0233b6287e2eca542d2d8f7e592c18f1a4ffeead
Sean P. Goggins <s@goggins.com>
2025-05-22 13:46:24 -0500

    updated logic
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- more_cowbell/index.sql
+++ more_cowbell/index.sql
@@ -15,3 +15,13 @@ ADD CONSTRAINT "fk_pull_request_message_ref_message_1" FOREIGN KEY ("msg_id") RE
 ALTER TABLE "augur_data"."pull_request_review_message_ref" 
   DROP CONSTRAINT "fk_pull_request_review_message_ref_pull_request_reviews_1",
   ADD CONSTRAINT "fk_pull_request_review_message_ref_pull_request_reviews_1" FOREIGN KEY ("pr_review_id") REFERENCES "augur_data"."pull_request_reviews" ("pr_review_id") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE "augur_data"."pull_requests" 
+  DROP CONSTRAINT "pull_requests_pr_augur_contributor_id_fkey",
+  ADD CONSTRAINT "pull_requests_pr_augur_contributor_id_fkey" FOREIGN KEY ("pr_augur_contributor_id") REFERENCES "augur_data"."contributors" ("cntrb_id") ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE "augur_data"."pull_request_events" 
+  DROP CONSTRAINT "fkprevent_repo_id",
+  DROP CONSTRAINT "pull_request_events_cntrb_id_fkey",
+  ADD CONSTRAINT "fkprevent_repo_id" FOREIGN KEY ("repo_id") REFERENCES "augur_data"."repo" ("repo_id") ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  ADD CONSTRAINT "pull_request_events_cntrb_id_fkey" FOREIGN KEY ("cntrb_id") REFERENCES "augur_data"."contributors" ("cntrb_id") ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;

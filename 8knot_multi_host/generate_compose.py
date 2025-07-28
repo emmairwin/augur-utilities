88a839a1550e15e42aaa332944bd1eda24c45fe3
Sean P. Goggins <s@goggins.com>
2025-07-27 22:11:30 -0500

    updated compose
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- 8knot_multi_host/generate_compose.py
+++ 8knot_multi_host/generate_compose.py
@@ -5,10 +5,32 @@ code_path = sys.argv[1] if len(sys.argv) > 1 else '.'
 
 os.makedirs("envs", exist_ok=True)
 
-placeholder = """# Environment for 8knot instance {i}
-API_KEY={{your value here}}
-REDIS_URL={{your value here}}
-WORKER_SCALE={{your value here}}
+placeholder = """# Environment for Augur instance {i}
+AUGUR_ADMIN_GROUPS_ENDPOINT={your value here}
+AUGUR_ADMIN_GROUP_NAMES_ENDPOINT={your value here}
+AUGUR_ADMIN_NAME_ENDPOINT={your value here}
+AUGUR_APP_ID={your value here}
+AUGUR_CLIENT_SECRET={your value here}
+AUGUR_DATABASE={your value here}
+AUGUR_HOST={your value here}
+AUGUR_LOGIN_ENABLED={your value here}
+AUGUR_PASSWORD={your value here}
+AUGUR_PORT={your value here}
+AUGUR_SCHEMA={your value here}
+AUGUR_SESSION_GENERATE_ENDPOINT={your value here}
+AUGUR_USERNAME={your value here}
+AUGUR_USER_ACCOUNT_ENDPOINT={your value here}
+AUGUR_USER_AUTH_ENDPOINT={your value here}
+AUGUR_USER_GROUPS_ENDPOINT={your value here}
+REDIS_PASSWORD={your value here}
+SECRET_KEY={your value here}
+OAUTH_CLIENT_NAME={your value here}
+OAUTH_CLIENT_ID={your value here}
+OAUTH_CLIENT_SECRET={your value here}
+OAUTH_AUTHORIZE_URL={your value here}
+OAUTH_TOKEN_URL={your value here}
+OAUTH_REDIRECT_URI={your value here}
+POSTGRES_PASSWORD={your value here}
 """
 
 template_header = "# Auto-generated multi-instance 8Knot compose\n# Use with `podman compose up -d`\n"
